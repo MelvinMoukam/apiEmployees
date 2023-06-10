0263bfc2cae85b53c4b286d1afcb199635e5d6f9
MelvinMoukam <moukammelvin@gmail.com>
2023-06-11 00:48:42 +0200

    classe test

---
+++ src/test/java/com/apiemployes/apiemployes/ApiEmployesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiemployes.apiemployes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEmployesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
