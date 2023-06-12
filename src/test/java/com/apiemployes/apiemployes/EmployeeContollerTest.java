dc47c673068ebdaec2441fe86b27cc53f525ff52
MelvinMoukam <moukammelvin@gmail.com>
2023-06-12 03:52:57 +0200

    test d'intégration

--- src/test/java/com/apiemployes/apiemployes/EmployeeContollerTest.java
+++ src/test/java/com/apiemployes/apiemployes/EmployeeContollerTest.java
@@ -1,16 +1,15 @@
 package com.apiemployes.apiemployes;
 
-import com.apiemployes.apiemployes.controller.EmployeeController;
-import com.apiemployes.apiemployes.service.EmployeeService;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
 
-import static net.bytebuddy.matcher.ElementMatchers.is;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 @SpringBootTest
@@ -21,12 +20,10 @@ public class EmployeeContollerTest {
     private MockMvc mockMvc;
 
 
-
     @Test
-    public void testGetEmployee () throws Exception {
-        mockMvc.perform(get("/employees")) // cette methode va déclencher la requete
-                .andExpect(status().isOk())
-                .andExpect((ResultMatcher) jsonPath("$[0].firstname" , is("Laurent")));
+    public void testGetEmployees() throws Exception {
+
+        mockMvc.perform(get("/employees")).andExpect(status().isOk()).andExpect(jsonPath("$[0].firstName", is("Laurent")));
 
     }
 }
