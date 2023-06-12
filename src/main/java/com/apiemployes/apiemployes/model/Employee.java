40b4b3bc828132c8140c906ecc90a8b888c0e2d6
MelvinMoukam <moukammelvin@gmail.com>
2023-06-12 04:20:20 +0200

    classe modèle

--- src/main/java/com/apiemployes/apiemployes/model/Employee.java
+++ src/main/java/com/apiemployes/apiemployes/model/Employee.java
@@ -20,8 +20,10 @@ public class Employee {
         @Column(name = "last_name")
         private String lastName ;
 
+        @Column(name = "mail")
         private String email;
 
+        @Column(name = "password")
         private String password;
 
 
